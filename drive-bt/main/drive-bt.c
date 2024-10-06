4801ecca362deedc2e84a2ed9a3a06d48fa1031a
gamroth-dawn <ngamroth@dawnequipment.com>
2024-10-06 09:29:12 -0500

    brought in example ble central code

--- drive-bt/main/drive-bt.c
+++ drive-bt/main/drive-bt.c
@@ -7,27 +7,36 @@
 #include "driver/gpio.h"
 #include "driver/ledc.h"
 
+
+
+#include "nimble/nimble_port.h"
+#include "nimble/nimble_port_freertos.h"
+#include "host/ble_hs.h"
+#include "host/util/util.h"
+#include "console/console.h"
+#include "services/gap/ble_svc_gap.h"
+#include "blecent.h"
+
 #define PWM
 
 #define TAG "drive-bt"
 
 #define LED1 GPIO_NUM_35
 #define LED2 GPIO_NUM_36
-// i reversed steer and drive outputs on the layout
-#define STEER_A GPIO_NUM_10
-#define STEER_B GPIO_NUM_9
-#define MOTOR_A GPIO_NUM_7
-#define MOTOR_B GPIO_NUM_6
 
-#define LEDC_TIMER              LEDC_TIMER_0
-#define LEDC_MODE               LEDC_LOW_SPEED_MODE
-//#define LEDC_OUTPUT_IO          (5) // Define the output GPIO
-#define LEDC_OUTPUT_IO          (7) // Define the output GPIO
-#define LEDC_CHANNEL            LEDC_CHANNEL_0
-#define LEDC_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
-#define LEDC_DUTY               (1096) // Set duty to 50%. (2 ** 13) * 50% = 4096
-#define LEDC_FREQUENCY          (200) // Frequency in Hertz. Set frequency at 4 kHz
+#define STEER_A GPIO_NUM_7
+#define STEER_B GPIO_NUM_6
+#define MOTOR_A GPIO_NUM_10
+#define MOTOR_B GPIO_NUM_9
 
+#define LEDC_TIMER LEDC_TIMER_0
+#define LEDC_MODE LEDC_LOW_SPEED_MODE
+// #define LEDC_OUTPUT_IO          (5) // Define the output GPIO
+#define LEDC_OUTPUT_IO (MOTOR_A) // Define the output GPIO
+#define LEDC_CHANNEL LEDC_CHANNEL_0
+#define LEDC_DUTY_RES LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
+#define LEDC_DUTY (1096)                // Set duty to 50%. (2 ** 13) * 50% = 4096
+#define LEDC_FREQUENCY (200)            // Frequency in Hertz. Set frequency at 4 kHz
 
 void init_io(void);
 
@@ -35,71 +44,73 @@ static void example_ledc_init(void)
 {
     // Prepare and then apply the LEDC PWM timer configuration
     ledc_timer_config_t ledc_timer = {
-        .speed_mode       = LEDC_MODE,
-        .duty_resolution  = LEDC_DUTY_RES,
-        .timer_num        = LEDC_TIMER,
-        .freq_hz          = LEDC_FREQUENCY,  // Set output frequency at 4 kHz
-        .clk_cfg          = LEDC_AUTO_CLK
-    };
+        .speed_mode = LEDC_MODE,
+        .duty_resolution = LEDC_DUTY_RES,
+        .timer_num = LEDC_TIMER,
+        .freq_hz = LEDC_FREQUENCY, // Set output frequency at 4 kHz
+        .clk_cfg = LEDC_AUTO_CLK};
     ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
 
     // Prepare and then apply the LEDC PWM channel configuration
     ledc_channel_config_t ledc_channel = {
-        .speed_mode     = LEDC_MODE,
-        .channel        = LEDC_CHANNEL,
-        .timer_sel      = LEDC_TIMER,
-        .intr_type      = LEDC_INTR_DISABLE,
-        .gpio_num       = LEDC_OUTPUT_IO,
-        .duty           = 4096, // Set duty to 0%
-        .hpoint         = 0
-    };
+        .speed_mode = LEDC_MODE,
+        .channel = LEDC_CHANNEL,
+        .timer_sel = LEDC_TIMER,
+        .intr_type = LEDC_INTR_DISABLE,
+        .gpio_num = LEDC_OUTPUT_IO,
+        .duty = 4096, // Set duty to 0%
+        .hpoint = 0};
     ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
 }
 
 void app_main(void)
 {
-	ESP_LOGI(TAG, "hello");
-	init_io();
+    ESP_LOGI(TAG, "hello");
+    init_io();
 
-	uint8_t led = 1;
-	gpio_set_level(MOTOR_A, 0);
-	gpio_set_level(MOTOR_B, 1);
+    uint8_t led = 1;
+    gpio_set_level(MOTOR_A, 0);
+    gpio_set_level(MOTOR_B, 0);
+    //gpio_set_level(MOTOR_B, 0);
 
-	gpio_set_level(STEER_A, 0);
-	gpio_set_level(STEER_B, 1);
+    gpio_set_level(STEER_A, 0);
+    gpio_set_level(STEER_B, 0);
 
 #ifdef PWM
-     // Set the LEDC peripheral configuration
+    // Set the LEDC peripheral configuration
     example_ledc_init();
     // Set duty to 50%
-    //ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY));
+    // ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY));
     // Update duty to apply the new value
-    //ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+    // ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
 
-        vTaskDelay(3000 / portTICK_PERIOD_MS);
+    init_ble();
+
+    // this is giving me 44% with no load
+    // uint32_t dc = 2500;
     uint32_t dc = 0;
     int32_t delta = 250;
     int32_t dir = 1;
-ESP_LOGI(TAG, "Setting to 4000");
+    /*vTaskDelay(3000 / portTICK_PERIOD_MS);
+    ESP_LOGI(TAG, "Setting to 4000");
     ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 4000));
     ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
-        vTaskDelay(3000 / portTICK_PERIOD_MS);
-ESP_LOGI(TAG, "Setting to 6000");
+    vTaskDelay(3000 / portTICK_PERIOD_MS);
+    ESP_LOGI(TAG, "Setting to 6000");
     ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 6000));
     ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
-        vTaskDelay(3000 / portTICK_PERIOD_MS);
-ESP_LOGI(TAG, "Setting to 8000");
+    vTaskDelay(3000 / portTICK_PERIOD_MS);
+    ESP_LOGI(TAG, "Setting to 8000");
     ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 8000));
     ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
-        vTaskDelay(3000 / portTICK_PERIOD_MS);
-        #endif
+    vTaskDelay(3000 / portTICK_PERIOD_MS);
+    */
+#endif
 
-	while(1) {
-		gpio_set_level(LED1, led);
-		gpio_set_level(LED2, !led);
-        //gpio_set_level(STEER_A, led);
-        //gpio_set_level(STEER_B, !led);
-        led = !led;
+    while (1)
+    {
+        gpio_set_level(LED1, led);
+        gpio_set_level(LED2, !led);
 #ifdef PWM
         ESP_LOGI(TAG, "setting dc to %lu", dc);
         ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, dc));
@@ -107,20 +118,28 @@ ESP_LOGI(TAG, "Setting to 8000");
         ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
 
         dc += (dir * delta);
-        if(dc > 7000) {
+        if (dc > 7000)
+        {
             dir = -1 * dir;
         }
-        if(dc < 250) {
+        if (dc < 250)
+        {
             dir = -1 * dir;
         }
-        #endif
+#else
+        gpio_set_level(STEER_A, led);
+        gpio_set_level(STEER_B, !led);
+        gpio_set_level(MOTOR_A, led);
+        gpio_set_level(MOTOR_B, !led);
+#endif
+        led = !led;
         vTaskDelay(1000 / portTICK_PERIOD_MS);
-	}
-
+    }
 }
 
-void init_io(void) {
-	gpio_config_t io_conf = {};
+void init_io(void)
+{
+    gpio_config_t io_conf = {};
     io_conf.intr_type = GPIO_INTR_DISABLE;
     io_conf.mode = GPIO_MODE_OUTPUT;
     io_conf.pin_bit_mask = (1ULL << LED1);
@@ -133,10 +152,10 @@ void init_io(void) {
     gpio_config(&io_conf);
     io_conf.pin_bit_mask = (1ULL << STEER_B);
     gpio_config(&io_conf);
-    #ifndef PWM
+#ifndef PWM
     io_conf.pin_bit_mask = (1ULL << MOTOR_A);
     gpio_config(&io_conf);
-    #endif
+#endif
     io_conf.pin_bit_mask = (1ULL << MOTOR_B);
     gpio_config(&io_conf);
 }
